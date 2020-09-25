09a372772cb0e76e268416fb2a0c58aa509f7743
Sergeyxow <59441462+Sergeyxow@users.noreply.github.com>
2020-09-25 16:29:54 +0300

    Add files via upload

---
+++ InputSystem.cs
@@ -0,0 +1,17 @@
+﻿using Leopotam.Ecs;
+using RingBoy.Components;
+using RingBoy.Configs;
+using UnityEngine;
+
+namespace RingBoy.Systems
+{
+    public class InputSystem : IEcsRunSystem
+    {
+        private RuntimeData _runtimeData;
+        private EcsWorld _world;
+
+        public void Run()
+        {
+        }
+    }
+}
---
+++ PlayerInput.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace RingBoy.Configs
+{
+    class PlayerInput
+    {
+        public Vector2 PositionFromAnchor;
+        public Vector2 FrameChange;
+        
+        public Vector2 AnchorPosition;
+        public Vector2 PreviousPosition;
+    }
+}
