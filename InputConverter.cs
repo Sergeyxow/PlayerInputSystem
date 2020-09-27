c364cbd6cd61b77644266f9bf3ac33994604c1ab
Sergeyxow <59441462+Sergeyxow@users.noreply.github.com>
2020-09-27 07:34:16 +0300

    Update InputConverter.cs

--- InputConverter.cs
+++ InputConverter.cs
@@ -1,45 +1,42 @@
 using UnityEngine;
 
-namespace TheSaboteur.Systems
+public class InputConverter
 {
-    public class InputConverter
+    private Vector3 _prevPos = Vector3.zero;
+    public Touch ConvertMouseToTouch()
     {
-        private Vector3 _prevPos = Vector3.zero;
-        public Touch ConvertMouseToTouch()
+        Touch touch = new Touch();
+
+        KeyCode lmb = KeyCode.Mouse0;
+
+        var mousePosition = Input.mousePosition;
+
+        if (Input.GetKeyDown(lmb))
         {
-            Touch touch = new Touch();
+            touch.phase = TouchPhase.Began;
 
-            KeyCode lmb = KeyCode.Mouse0;
-
-            var mousePosition = Input.mousePosition;
-            
-            if (Input.GetKeyDown(lmb))
-            {
-                touch.phase = TouchPhase.Began;
-                
-                _prevPos = Vector3.zero;
-            }
-            else if (Input.GetKey(lmb))
-            {
-                if (mousePosition != _prevPos)
-                    touch.phase = TouchPhase.Moved;
-                else
-                    touch.phase = TouchPhase.Stationary;
-
-                _prevPos = mousePosition;
-            }
-            else if (Input.GetKeyUp(lmb))
-            {
-                touch.phase = TouchPhase.Ended;
-            }
-            else
-            {
-                touch.phase = TouchPhase.Canceled;
-            }
-
-            touch.position = mousePosition;
-
-            return touch;
+            _prevPos = Vector3.zero;
         }
+        else if (Input.GetKey(lmb))
+        {
+            if (mousePosition != _prevPos)
+                touch.phase = TouchPhase.Moved;
+            else
+                touch.phase = TouchPhase.Stationary;
+
+            _prevPos = mousePosition;
+        }
+        else if (Input.GetKeyUp(lmb))
+        {
+            touch.phase = TouchPhase.Ended;
+        }
+        else
+        {
+            touch.phase = TouchPhase.Canceled;
+        }
+
+        touch.position = mousePosition;
+
+        return touch;
     }
 }
