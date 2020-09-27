05f9a64ce9b5a8d56f9431e878e9c1b6d4ab58f9
Sergeyxow <59441462+Sergeyxow@users.noreply.github.com>
2020-09-27 07:34:44 +0300

    Update PlayerInput.cs

--- PlayerInput.cs
+++ PlayerInput.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 
-namespace RingBoy.Configs
+class PlayerInput
 {
-    class PlayerInput
-    {
-        public Vector2 PositionFromAnchor;
-        public Vector2 FrameChange;
-        
-        public Vector2 AnchorPosition;
-        public Vector2 PreviousPosition;
-    }
+    public Vector2 PositionFromAnchor;
+    public Vector2 FrameChange;
+
+    public Vector2 AnchorPosition;
+    public Vector2 PreviousPosition;
 }
